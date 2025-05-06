79aa727e7bdaf41c109ef84bdf75510090dfde26
VincenzoAmari <vincenzo.am99@gmail.com>
2025-05-06 12:29:01 +0200

    snack 1 no bonus completo

--- main.js
+++ main.js
@@ -3,17 +3,24 @@ async function getChefBirthday(id) {
     const recipeResponse = await fetch(`https://dummyjson.com/recipes/${id}`);
     const recipeData = await recipeResponse.json();
 
-    console.log(recipeData);
+    // console.log(recipeData);
+
     const userId = recipeData.userId;
-    console.log("User ID:", userId);
+    // console.log("User ID:", userId);
+
     const userResponse = await fetch(`https://dummyjson.com/users/${userId}`);
     const userData = await userResponse.json();
+
     const birthDate = userData.birthDate;
-    console.log("Data di nascita dello chef:", birthDate);
-    console.log(userData);
+    return birthDate;
+    // console.log("Data di nascita dello chef:", birthDate);
+    // console.log(userData);
   } catch (error) {
     console.error("errore nel recupero della ricetta", error);
+    throw error;
   }
 }
 
-getChefBirthday(1);
+getChefBirthday(1).then((date) =>
+  console.log("data di nascita dello chef è:", date)
+);

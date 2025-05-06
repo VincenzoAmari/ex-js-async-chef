d5bf18e5741d692576b4372b997741cd99fa0de1
VincenzoAmari <vincenzo.am99@gmail.com>
2025-05-06 12:48:47 +0200

    aggiunti bonus e indexhtml per testarlo

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
+import dayjs from "dayjs";
+
 async function getChefBirthday(id) {
   try {
     const recipeResponse = await fetch(`https://dummyjson.com/recipes/${id}`);
+    if (!recipeResponse.ok) {
+      throw new Error(`Ricetta con id ${id} non trovata`);
+    }
+
     const recipeData = await recipeResponse.json();
-
     // console.log(recipeData);
-
     const userId = recipeData.userId;
     // console.log("User ID:", userId);
 
     const userResponse = await fetch(`https://dummyjson.com/users/${userId}`);
+    if (!userResponse.ok) {
+      throw new Error(`Utente con id ${userId} non trovato`);
+    }
     const userData = await userResponse.json();
 
     const birthDate = userData.birthDate;
-    return birthDate;
+    const formattedDate = dayjs(birthDate).format("DD/MM/YYYY");
+
+    return formattedDate;
     // console.log("Data di nascita dello chef:", birthDate);
     // console.log(userData);
   } catch (error) {
